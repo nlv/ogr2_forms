462aebdb65dddb7f9ce9f7ce2a91a0af5707bc79
Leon Nikitin <nlv@lab321.ru>
2018-03-29 23:04:45 +0600

    страница ВК по умолчанию в списке

--- manifest.php
+++ manifest.php
@@ -6,9 +6,9 @@ $manifest = array(
     'author' => 'Leon.V.Nikitin@pravmail.ru',
     'description' => 'Формы для org2.',
     'is_uninstallable' => true,
-    'published_date' => '2018-03-28',
+    'published_date' => '2018-03-29',
     'type' => 'module',
-    'version' => '1.0.4',
+    'version' => '1.0.6',
     'dependencies' => array(
       array(
        'id_name' => 'org2_contacts',
